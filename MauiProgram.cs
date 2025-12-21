e8a5f0c633fbe2222dca07a0781d17a05007e526
Noble Nepal <nepalnoble525@gmail.com>
2025-12-21 10:49:19 +0545

    Login Authentication Completion

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,7 +2,7 @@
 using BhawanaPatra.Service;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using MudBlazor;
 using MudBlazor.Services;
 using SQLite;
 namespace BhawanaPatra
@@ -23,7 +23,7 @@ namespace BhawanaPatra
 
             // Register UserService for DI
             builder.Services.AddScoped<UserService>();
-
+            
 
             //Razor and MudBlazor
             builder.Services.AddMauiBlazorWebView();
@@ -31,6 +31,14 @@ namespace BhawanaPatra
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 3000;
+            });
+            
 #endif
 
             return builder.Build();
