2b1a491d4a4a5cef038d326c1311dd526102d4f1
Noble Nepal <nepalnoble525@gmail.com>
2026-01-26 10:38:27 +0545

    Creation of StreakAnalyticsService for Modularity in EntryService class

---
+++ Service/StreakAnalyticsService.cs
@@ -0,0 +1,19 @@
+﻿using BhawanaPatra.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BhawanaPatra.Service
+{
+    public class StreakAnalyticsService
+    {
+        private readonly DatabaseConfiguration _db;
+
+        public StreakAnalyticsService(DatabaseConfiguration db)
+        {
+            _db = db;
+        }
+    }
+}
